54a0df5098f2b8a5009197e9cb9fce6263eaf9cb
pawas-saxena-paragon <pawas.saxena+1@gmail.com>
2023-11-02 10:31:26 +0530

    n Queen chatgpt code

--- nQueen.ts
+++ nQueen.ts
@@ -1,61 +1,67 @@
-// https://leetcode.com/problems/n-queens/
-// incomplete code. unable to solve this so moving on
-export function solveNQueens(n: number): string[][] {
-  // explore all paths
-  // rowCheck = no two queens are on the same row
-  // colCheck = no two queens are on the same row
-  // diagonal check = no two queens are on the same diagonal
-  // isSafe = rowCheck && colCheck && diagonalCheck
-  // will place queens on new col and no two queen in the same row and later check the bounding fn on the diagonal condn
+// chatgpt code
+class NQueens {
+  private board: number[][];
+  private solutions: number[][][];
 
-  const row = Array(n).fill(0);
-  const board: number[][] = Array(n).fill(row);
+  constructor(private N: number) {
+    this.board = new Array(N);
+    for (let i = 0; i < N; i++) {
+      this.board[i] = new Array(N).fill(0);
+    }
+    this.solutions = [];
+  }
 
-  //placing the queen at 0,0
-  solve(n, board, [n, n]);
-}
+  solveNQueens(): number[][][] {
+    this.placeQueens(0);
+    return this.solutions;
+  }
 
-function solve(
-  n: number,
-  board: number[][],
-  [width, height]: [number, number]
-): number[][] | null {
-  if (n === 0) {
-    // no queens to place so return the board
-    return board;
-  } else if (n === 1 && width === 1 && height === 1) {
-    return [[1]];
-  } else {
-    // place the queen in some cell and check the safety condition
-    for (let i = 0; i < width; i++) {
-      
+  placeQueens(row: number): void {
+    if (row === this.N) {
+      const solution: number[][] = [];
+      for (let i = 0; i < this.N; i++) {
+        solution.push([...this.board[i]]);
+      }
+      this.solutions.push(solution);
+      return;
+    }
+
+    for (let col = 0; col < this.N; col++) {
+      if (this.isSafe(row, col)) {
+        this.board[row][col] = 1;
+        this.placeQueens(row + 1);
+        this.board[row][col] = 0;
+      }
     }
   }
 
-  // since we are placing the queen in a row and starting from the first row, the height of the board will decrease
-  // using the height variable we will place the queen in a row in a loop
-  //    we will place the queen in a available column starting from left to right 
-  //    for each col we will check if the col is safe 
-  //    if the col is not safe we move to next col
-  //    if the col is safe we will place the queen in the col and reduce the height and call the solve fn again 
-  //    if the solve fn returns a value I will add the board to the results 
-  //    the board is the shared variable and its value will keep on changing 
-  //    but we are moving one col at a time within a row. We are moving sequentially so it will not be an issue
-  //    I will have to clear the board before proceeding to the next safe column
-  //    If the solve fn returns a  null value then I will backtrack and move to next column. Also clear the board for a height 
-  
+  isSafe(row: number, col: number): boolean {
+    for (let i = 0; i < row; i++) {
+      if (this.board[i][col] === 1) {
+        return false; // Check column
+      }
+
+      if (col - row + i >= 0 && this.board[i][col - row + i] === 1) {
+        return false; // Check left diagonal
+      }
+
+      if (col + row - i < this.N && this.board[i][col + row - i] === 1) {
+        return false; // Check right diagonal
+      }
+    }
+
+    return true;
+  }
 }
 
-/**
- * cover all cells and check for safety condition
- * move diagonally can perform optimization
- * optimize so that the same cell is not checked again and again
- * @param board
- */
-function isSafe(board: number[][]) {}
-
-function stringifySoln(board: number[][]): string[] {
-  return board.map((val: number[]) =>
-    val.map((cellVal: number) => (cellVal === 1 ? "Q" : ".")).join("")
-  );
-}
+// Example usage:
+const n = 4; // Change this to the desired N value
+const nQueens = new NQueens(n);
+const solutions = nQueens.solveNQueens();
+solutions.forEach((solution, index) => {
+  console.log(`Solution ${index + 1}:`);
+  solution.forEach(row => {
+    console.log(row.map(cell => (cell === 1 ? 'Q' : '.')).join(' '));
+  });
+  console.log('\n');
+});
