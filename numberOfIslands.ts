ef3629393f16eb248e365bb1a50dc9bfc4629434
pawas-saxena-paragon <pawas.saxena+1@gmail.com>
2023-11-04 13:55:52 +0530

    numIslands correct soln

--- numberOfIslands.ts
+++ numberOfIslands.ts
@@ -3,107 +3,53 @@ export function numIslands(grid: string[][]): number {
   const numIslands = new NumIslands(grid);
   numIslands.solve();
 
-  return numIslands.islands.length;
+  return numIslands.count;
 }
 
 type Point = [number, number];
-type Island = Point[];
 
 class NumIslands {
   Nx: number;
   Ny: number;
-  islands: Island[];
+  count: number;
 
-  constructor(private gird: string[][]) {
-    this.Nx = this.gird[0].length;
-    this.Ny = this.gird.length;
-    this.islands = [];
+  constructor(private grid: string[][]) {
+    this.Nx = this.grid.length;
+    this.Ny = this.grid[0].length;
+    this.count = 0;
   }
 
-  getNextMoves(x: number, y: number): Point[] {
-    const allPossibleNextMoves: Point[] = [
-      [x + 1, y],
-      [x - 1, y],
-      [x, y + 1],
-      [x, y - 1],
-      // [x + 1, y + 1],
-      // [x + 1, y - 1],
-      // [x - 1, y + 1],
-      // [x - 1, y - 1],
-    ];
-
-    return allPossibleNextMoves.filter(
-      ([x, y]: Point) =>
-        x >= 0 &&
-        x < this.Nx &&
-        y >= 0 &&
-        y < this.Ny &&
-        this.gird[x][y] === "1"
-    );
-  }
-
-  searchInFoundIslands(x: number, y: number): boolean {
-    const foundIsland = this.islands.find((island: Island) => {
-      return island.find(([_x, _y]: Point) => _x === x && _y === y);
-    });
-
-    return Boolean(foundIsland);
-  }
-
-  findLandNode([x, y]: Point = [0, 0]): Point | null {
-    for (let i = x; i < this.Nx; i++) {
-      for (let j = y; i < this.Ny; j++) {
-        if (
-          this.gird[i][j] === "1" &&
-          this.searchInFoundIslands(i, j) === false
-        ) {
-          return [i, j];
+  solve() {
+    for (let i = 0; i < this.Nx; i++) {
+      for (let j = 0; j < this.Ny; j++) {
+        if (this.grid[i][j] === "1") {
+          // travese and set 0 an island
+          this.dfs([i, j]);
+          this.count++;
         }
       }
     }
-
-    return null;
   }
 
   /**
-   * 
-   * @param param0 this function runs on forever
-   * @returns 
+   * traverses 1 island to all boundaries and change visited nodes back to '0'
    */
-  solveForOneIsland([x, y]: Point): Island {
-    const island: Island = [];
-    const nextMoves: Point[] = [this.findLandNode([x, y])];
-    while (nextMoves.length) {
-      console.log("nextMoves", nextMoves.length);
-      const newLandNode = nextMoves.shift();
-      island.push(newLandNode);
-
-      const _next = this.getNextMoves(newLandNode[0], newLandNode[1]);
-      _next.filter(
-        ([_x, _y]: Point) =>
-          nextMoves.find(([x1, y1]) => _x === x1 && _y === y1) === undefined
-      );
-      nextMoves.push(..._next);
+  dfs([x, y]: Point): void {
+    if (
+      x < 0 ||
+      x >= this.Nx ||
+      y < 0 ||
+      y >= this.Ny ||
+      this.grid[x][y] === "0"
+    ) {
+      return;
     }
 
-    return island;
-  }
+    this.grid[x][y] = "0";
 
-  solve(): void {
-    let [startx, starty]: Point = [-1, 0];
-    // let landNode = this.findLandNode([startx, starty]);
-
-    let landNode = null;
-    do {
-      if (startx < this.Nx) {
-        startx++;
-      } else {
-        starty++;
-      }
-      console.log("solving fir", [startx, starty]);
-
-      landNode = this.findLandNode([startx, starty]);
-      this.islands.push(this.solveForOneIsland(landNode));
-    } while (landNode);
+    this.dfs([x + 1, y]);
+    this.dfs([x - 1, y]);
+    this.dfs([x, y + 1]);
+    this.dfs([x, y - 1]);
   }
 }
