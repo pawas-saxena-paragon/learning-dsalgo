e0c3acc43553da67e56bf3c98db53bcf6b810284
pawas-saxena-paragon <pawas.saxena+1@gmail.com>
2023-12-03 07:18:21 +0530

    added DP to longest increasing subsequence

--- longestIncreasingSubsequence.ts
+++ longestIncreasingSubsequence.ts
@@ -1,30 +1,41 @@
 // https://leetcode.com/problems/longest-increasing-subsequence/?envType=study-plan-v2&envId=top-interview-150
 
-// const dp: number[][] = [];
+const dp: number[][] = [];
 export function lengthOfLIS(nums: number[]): number {
-  // for (let i = 0; i < nums.length; i++) {
-  //   dp[i] = [];
-  // }
-  return solve(nums, nums.length, Infinity);
+  for (let i = 0; i < nums.length; i++) {
+    dp[i] = [];
+  }
+  return solve(nums, nums.length, -1);
 }
 
 function solve(
   nums: number[],
   n: number,
-  minElementOfSubsequenceTillNow: number
+  indexMinElementOfSubsequenceTillNow: number
 ): number {
   if (n === 0) {
     return 0;
   }
 
-  if (nums[n - 1] < minElementOfSubsequenceTillNow) {
+  if (dp[n - 1][indexMinElementOfSubsequenceTillNow] !== undefined) {
+    return dp[n - 1][indexMinElementOfSubsequenceTillNow];
+  }
+
+  if (
+    nums[n - 1] < nums[indexMinElementOfSubsequenceTillNow] ||
+    indexMinElementOfSubsequenceTillNow === -1
+  ) {
     // take the current element and add it to the subsequence.
-    return Math.max(
-      1 + solve(nums, n - 1, nums[n - 1]),
-      solve(nums, n - 1, minElementOfSubsequenceTillNow)
-    );
+    return (dp[n - 1][indexMinElementOfSubsequenceTillNow] = Math.max(
+      1 + solve(nums, n - 1, n - 1),
+      solve(nums, n - 1, indexMinElementOfSubsequenceTillNow)
+    ));
   } else {
     // not taking the nth element as part of the subsequence
-    return solve(nums, n - 1, minElementOfSubsequenceTillNow);
+    return (dp[n - 1][indexMinElementOfSubsequenceTillNow] = solve(
+      nums,
+      n - 1,
+      indexMinElementOfSubsequenceTillNow
+    ));
   }
 }
