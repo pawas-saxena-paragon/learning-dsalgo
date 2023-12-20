30f424381c4c714be0fb8f27c771e8c5ffb7de50
pawas-saxena-paragon <pawas.saxena+1@gmail.com>
2023-12-20 12:56:26 +0530

    unique path dp

--- lc62.uniquePaths.ts
+++ lc62.uniquePaths.ts
@@ -1,32 +1,39 @@
 // https://leetcode.com/problems/unique-paths/?envType=list&envId=pm1gvxgg
 export function uniquePaths(m: number, n: number): number {
   const sol = new Solution(m, n);
-  return sol.numberOfWays;
+  return sol.dfs([0, 0]);
 }
 
 export class Solution {
   numberOfWays: number;
+  dp: number[][];
   constructor(private nRows: number, private nCols: number) {
-    this.numberOfWays = 0;
-    this.dfs([0, 0]);
+    this.dp = [];
+    for (let i = 0; i < nRows + 1; i++) {
+      const row = new Array(nCols + 1).fill(-1);
+      this.dp.push(row);
+    }
   }
 
-  dfs([startx, starty]: [number, number]): void {
-    if (startx > this.nCols - 1 || starty > this.nRows - 1) {
-      return;
+  dfs([startx, starty]: [number, number]): number {
+    if (startx > this.nRows - 1 || starty > this.nCols - 1) {
+      return 0;
     }
 
     if (this.isDestination([startx, starty])) {
-      this.numberOfWays++;
-      return;
+      return 1;
     }
 
-    this.dfs([startx + 1, starty]);
-    this.dfs([startx, starty + 1]);
+    if (this.dp[startx][starty] !== -1) {
+      return this.dp[startx][starty];
+    }
+
+    return (this.dp[startx][starty] =
+      this.dfs([startx + 1, starty]) + this.dfs([startx, starty + 1]));
   }
 
   isDestination([x, y]: [number, number]): boolean {
-    return Boolean(x === this.nCols - 1 && y === this.nRows-1);
+    return Boolean(x === this.nRows - 1 && y === this.nCols - 1);
   }
 }
 
@@ -34,4 +41,6 @@ export class Solution {
  * kind of like a matrix dfs in graphs
  * can move in only 2 dirs
  * are these ways unique ? looks unique
+ *
+ * this will count cells i think
  */
