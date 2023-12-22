325039124679e1d47089f1177b78f42d61ff2294
pawas-saxena-paragon <pawas.saxena+1@gmail.com>
2023-12-22 08:15:59 +0530

    first last position binary search

--- firstLastPosition.ts
+++ firstLastPosition.ts
@@ -4,39 +4,49 @@ export function searchRange(nums: number[], target: number): number[] {
   if (nums.length === 0) {
     return [-1, -1];
   }
+  if (nums.length === 1) {
+    return nums[0] === target ? [0, 0] : [-1, -1];
+  }
 
+  const result: number[] = [];
   let start = 0;
   let end = nums.length - 1;
-  const positions = [];
+  let foundPos: number;
 
   while (start <= end) {
-    console.log("start", start, "end", end);
-    let mid = Math.floor((start + end) / 2);
+    let mid = Math.floor(start + (end - start) / 2);
     if (nums[mid] === target) {
-      positions.push(mid); // can remove this push
-      // move to left untill element is different than mid
-      let i = mid;
-      while (nums[i] === target) {
-        positions.push(i);
-        i--;
-      }
-      // move to right until elemnet is different than mid
-      i = mid;
-      while (nums[i] === target) {
-        positions.push(i);
-        i++;
-      }
+      foundPos = mid;
       break;
-    } else if (nums[mid] < target) {
-      // choose right arr
-      start = mid + 1;
     } else if (nums[mid] > target) {
-      // choose left arr
       end = mid - 1;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
     }
   }
 
-  return positions.length
-    ? [Math.min(...positions), Math.max(...positions)]
-    : [-1, -1];
+  if (foundPos === undefined) {
+    return [-1, -1];
+  }
+
+  let i = foundPos;
+  while (nums[i] === target) {
+    i++;
+  }
+  result[1] = i - 1;
+
+  i = foundPos;
+  while (nums[i] === target) {
+    i--;
+  }
+  result[0] = i + 1;
+
+  return result;
 }
+
+/**
+ * find first occurence. Keep moving forward until the number changes
+ *
+ *
+ * considering when start and end point to the same element to be invalid cond.
+ */
