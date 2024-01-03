e03528f830c849a75d3cd9ef9f49083eed0efed7
pawas-saxena-paragon <pawas.saxena+1@gmail.com>
2024-01-03 14:02:24 +0530

    added bfs to binary tree utils

--- src/binaryTree.util.ts
+++ src/binaryTree.util.ts
@@ -8,3 +8,34 @@ export class TreeNode {
     this.right = right === undefined ? null : right;
   }
 }
+
+type NodeLevel = { node: TreeNode; level: number };
+
+function bfs(root: TreeNode): TreeNode[][] {
+  const resultWithLevels: NodeLevel[] = [];
+  const toVist: NodeLevel[] = [{ node: root, level: 0 }];
+
+  while (toVist.length !== 0) {
+    let { node: currentTreeNode, level } = toVist.shift();
+    resultWithLevels.push({ node: currentTreeNode, level });
+    if (currentTreeNode.left !== null) {
+      toVist.push({ node: currentTreeNode.left, level: level + 1 });
+    }
+
+    if (currentTreeNode.right !== null) {
+      toVist.push({ node: currentTreeNode.right, level: level + 1 });
+    }
+  }
+
+  return resultWithLevels.reduce(
+    (acc: TreeNode[][], { node, level }: NodeLevel) => {
+      if (acc[level]) {
+        acc[level].push(node);
+      } else {
+        acc[level] = [node];
+      }
+      return acc;
+    },
+    [] as TreeNode[][]
+  );
+}
