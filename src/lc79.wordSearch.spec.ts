d8eb205743a1286eb20c044f000e1f5dd10d18a1
pawas-saxena-paragon <pawas.saxena+1@gmail.com>
2024-01-07 19:15:19 +0530

    fixed issue with word search . The current soln gives TLE for some inputs but passes almost all test cases.

--- src/lc79.wordSearch.spec.ts
+++ src/lc79.wordSearch.spec.ts
@@ -5,6 +5,13 @@ const board = [
   ["S", "F", "C", "S"],
   ["A", "D", "E", "E"],
 ];
+
+const board1 = [
+  ["A", "B", "C", "E"],
+  ["S", "F", "E", "S"],
+  ["A", "D", "E", "E"],
+];
+
 describe("wordSearch", () => {
   it("ABCCED", () => {
     expect(exist(board, "ABCCED")).toEqual(true);
@@ -16,5 +23,17 @@ describe("wordSearch", () => {
 
   it("ABCB", () => {
     expect(exist(board, "ABCB")).toEqual(false);
+  });
+
+  it('[["a"]], word = "ab"', () => {
+    expect(exist([["a"]], "ab")).toEqual(false);
+  });
+
+  it('[["a"]], a', () => {
+    expect(exist([["a"]], "a")).toEqual(true);
+  });
+
+  it('"ABCESEEEFS"', () => {
+    expect(exist(board1, "ABCESEEEFS")).toEqual(true);
   });
 });
--- src/lc79.wordSearch.ts
+++ src/lc79.wordSearch.ts
@@ -1,3 +1,127 @@
+// https://leetcode.com/problems/word-search/?envType=study-plan-v2&envId=top-interview-150
 export function exist(board: string[][], word: string): boolean {
-    
-};
+  const sol = new Solution(board, word);
+  return sol.solve();
+}
+
+/**
+ * [rowNum, colNum]
+ * this is not same as caretisan coordinates as there [x, y] represents [colNum, rowNum]
+ */
+type Point = [number, number];
+
+class Solution {
+  nRow: number;
+  nCol: number;
+  directions: Point[];
+  startPoint: Point[];
+
+  constructor(private board: string[][], private word: string) {
+    this.nRow = this.board.length;
+    this.nCol = this.board[0].length;
+    this.startPoint = [];
+    this.directions = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+    ];
+    this.initStartPoint();
+  }
+
+  /**
+   *
+   * @param rowNum i is the row num. Outer loop
+   * @param colNum j is the col num. inner loop
+   */
+  isOutBound(rowNum: number, colNum: number): boolean {
+    return Boolean(
+      rowNum < 0 || rowNum >= this.nRow || colNum < 0 || colNum >= this.nCol
+    );
+  }
+
+  initStartPoint(): void {
+    for (let i = 0; i < this.nRow; i++) {
+      for (let j = 0; j < this.nCol; j++) {
+        if (this.board[i][j] === this.word.charAt(0)) {
+          // word is at least length 1 as per the constraints
+          this.startPoint.push([i, j]);
+        }
+      }
+    }
+  }
+
+  dfs(
+    rowNum: number,
+    colNum: number,
+    _word: string,
+    _visited: Point[]
+  ): boolean {
+    console.log(
+      "-----------------------",
+      `curretn ${rowNum}${colNum}`,
+      this.board[rowNum][colNum],
+      'cuurentWord',
+      _word,
+      "visited",
+      _visited.map(([i, j]: Point) => this.board[i][j]).join("")
+    );
+
+    if (_word.length === 0) {
+      return true;
+    }
+    if (this.board[rowNum][colNum] !== _word.charAt(0)) {
+      return false;
+    }
+
+    const nextCells: Point[] = this.directions
+      .map(([diri, dirj]: Point) => [rowNum + diri, colNum + dirj] as Point)
+      .filter(([ni, nj]: Point) => !this.isOutBound(ni, nj))
+      .filter(
+        ([ni, nj]: Point) =>
+          !Boolean(_visited.find(([vi, vj]: Point) => vi === ni && vj === nj))
+      );
+
+    _visited.push([rowNum, colNum]);
+
+    if (nextCells.length === 0) {
+      return (
+        this.board[rowNum][colNum] === _word.charAt(0) && _word.length === 1
+      );
+    }
+
+    return nextCells
+      .map(([ni, nj]: Point) => {
+        return this.dfs(
+          ni,
+          nj,
+          _word.length === 1 ? "" : _word.slice(1),
+          _visited.slice()
+        );
+      })
+      .reduce((acc: boolean, curr: boolean) => {
+        return acc || curr;
+      });
+  }
+
+  solve(): boolean {
+    if (this.startPoint.length === 0) {
+      return false;
+    }
+
+    for (let [si, sj] of this.startPoint) {
+      const isFound = this.dfs(si, sj, this.word, []);
+      if (isFound === true) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
+
+/**
+ * when the word arr becomes empty then we have found the soln.
+ * find potential start points.
+ * then traverse each start point for the soln.
+ */
