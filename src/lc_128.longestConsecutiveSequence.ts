1ff2e9a4cf22f2b7930a57bcf3d851ecec8603fd
pawas-saxena-paragon <pawas.saxena+1@gmail.com>
2024-08-30 22:37:26 +0530

    longest consecutive sequence

---
+++ src/lc_128.longestConsecutiveSequence.ts
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/description/?envType=study-plan-v2&envId=top-interview-150
+
+function longestConsecutive(nums: number[]): number {
+  // will do later.
+}
