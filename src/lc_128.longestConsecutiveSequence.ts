65b0142c8f9ae6af9f07e8a8314a4fa436dd0367
pawas-saxena-paragon <pawas.saxena+1@gmail.com>
2024-09-12 22:54:11 +0530

    lc_128 longest consecutive sequence TLE soln

--- src/lc_128.longestConsecutiveSequence.ts
+++ src/lc_128.longestConsecutiveSequence.ts
@@ -1,5 +1,24 @@
 // https://leetcode.com/problems/longest-consecutive-sequence/description/?envType=study-plan-v2&envId=top-interview-150
 
 function longestConsecutive(nums: number[]): number {
-  // will do later.
+  const mySet = new Set(nums);
+
+  let maxLength = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let maxLengthCurrent = 1;
+    let currentNum = nums[i];
+    while (mySet.has(currentNum + 1)) {
+      currentNum++;
+      maxLengthCurrent++;
+    }
+    currentNum = nums[i];
+    while (mySet.has(currentNum - 1)) {
+      currentNum--;
+      maxLengthCurrent++;
+    }
+
+    maxLength = Math.max(maxLength, maxLengthCurrent);
+  }
+
+  return maxLength;
 }
